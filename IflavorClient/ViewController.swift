600277be1109c406d968d7fd773c28dea07a8a12
三浦　優平 <miura.yu.0702@gmail.com>
2017-11-17 22:55:40 +0900

    create xcode project

---
+++ IflavorClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IflavorClient
+//
+//  Copyright © 2017年 bitswitches. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
